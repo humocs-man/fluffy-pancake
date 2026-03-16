439ba38045087c8dcec33750e35fa25a7d36bb51
humocs-man <251756307+humocs-man@users.noreply.github.com>
2026-03-16 11:20:27 +0100

    Update install-homebrew.sh

--- files/usr/local/bin/install-homebrew.sh
+++ files/usr/local/bin/install-homebrew.sh
@@ -6,12 +6,13 @@ HOME="/var/home/$USER"
 export HOME
 export NONINTERACTIVE=1
 
-
 # Homebrew installieren (als User, aber ohne Interaktion)
-sudo -u "$USER" HOME="$HOME" NONINTERACTIVE=1 bash -c \
-  '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+HOME="$HOME" NONINTERACTIVE=1 bash -c '
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" --prefix="$HOME/.linuxbrew"
+'
 
 # Optional: Brew in PATH eintragen
 if ! grep -q 'linuxbrew' "$HOME/.bashrc"; then
-  echo 'eval "$(/var/home/'"$USER"'/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.bashrc"
+  echo 'eval "$($HOME/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.bashrc"
 fi
+
